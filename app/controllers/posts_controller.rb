8ecf059a016f67a5911f4f93ddaba1a9167cae6d
Mohammed <moohyacc@gmail.com>
2021-01-20 00:01:02 +0300

    post fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.near_by_posts request.location.coordinates
+    @posts = Post.near_by_posts request.location.city
     puts @posts
   end
 
