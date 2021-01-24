0035322301759a56494be044ec610f8503725abf
Mohammed <moohyacc@gmail.com>
2021-01-25 02:54:16 +0300

    js

--- app/mailers/new_comment_mailer.rb
+++ app/mailers/new_comment_mailer.rb
@@ -2,7 +2,7 @@ class NewCommentMailer < ApplicationMailer
     default from: 'no-reply@passing-by.com'
 
     def email_owner(post_id)
-        @post = Post.find_by(id:post_id)
+        @post = Post.find(id: post_id)
         @owner = @post.user.email
         mail(to: @owner, subject: "New comment to post ##{@post.id}",
             template_path: 'new_comment_mailer',
