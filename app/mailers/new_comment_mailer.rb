31298ff3aa8485135f4e26200d91f3e4db48beae
Mohammed <moohyacc@gmail.com>
2021-01-25 03:20:47 +0300

    js

--- app/mailers/new_comment_mailer.rb
+++ app/mailers/new_comment_mailer.rb
@@ -4,7 +4,7 @@ class NewCommentMailer < ApplicationMailer
     def self.email_owner(post_id)
         @post = Post.find_by(id: post_id)
         owner = @post.user.email
-        mail(to: owner, subject: "New comment to post ##{post_id}",
+        ActionMailer::Base.mail(to: owner, subject: "New comment to post ##{post_id}",
             template_path: 'new_comment_mailer',
             template_name: 'new_comment_mailer')
     end
