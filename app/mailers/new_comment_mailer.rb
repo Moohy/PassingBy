9ff9d3eb67a0923cfc7db3111064d61df6fc88c9
Mohammed <moohyacc@gmail.com>
2021-01-26 18:59:33 +0300

    conf

--- app/mailers/new_comment_mailer.rb
+++ app/mailers/new_comment_mailer.rb
@@ -9,8 +9,13 @@ class NewCommentMailer < ApplicationMailer
 
         owner = @post.user.email
         puts "########### #{owner}"
-        ActionMailer::Base.mail(to: "mode99910@gmail.com", subject: "New comment to post ##{post_id}",
-            template_path: 'new_comment_mailer',
-            template_name: 'new_comment_mailer')
+        # ActionMailer::Base.mail(to: "mode99910@gmail.com", subject: "New comment to post ##{post_id}",
+        #     template_path: 'new_comment_mailer',
+        #     template_name: 'new_comment_mailer')
+
+        mail(to: "mode99910@gmail.com",
+            subject: 'Welcome to passingBy') do |format|
+            format.text { render plain: 'New comment to post ##{post_id}"' }
+        end
     end
 end
