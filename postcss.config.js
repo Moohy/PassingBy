cf172efbf0e8fd83292937467b64ca39ca524834
Mohammed <moohyacc@gmail.com>
2021-01-28 21:37:46 +0300

    unlike fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  plugins: [
-  require('./app/javascript/packs/tailwind.config.js'),
-	require('tailwindcss'),
-	  require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
-}
+// module.exports = {
+//   plugins: [
+//   require('./app/javascript/packs/tailwind.config.js'),
+// 	require('tailwindcss'),
+// 	  require('postcss-import'),
+//     require('postcss-flexbugs-fixes'),
+//     require('postcss-preset-env')({
+//       autoprefixer: {
+//         flexbox: 'no-2009'
+//       },
+//       stage: 3
+//     })
+//   ]
+// }
